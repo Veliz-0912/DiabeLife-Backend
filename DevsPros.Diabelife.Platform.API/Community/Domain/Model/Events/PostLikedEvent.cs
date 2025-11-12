af15c73f92683927890f2442a2d67a76a4fe98c1
Gabriel Mamani <u202220659@upc.edu.pe>
2025-11-11 21:12:20 -0500

    feat: add post liked event

--- DevsPros.Diabelife.Platform.API/Community/Domain/Model/Events/PostLikedEvent.cs
+++ DevsPros.Diabelife.Platform.API/Community/Domain/Model/Events/PostLikedEvent.cs
@@ -1,6 +1,9 @@
+using DevsPros.Diabelife.Platform.API.Shared.Domain.Model.Events;
+
 namespace DevsPros.Diabelife.Platform.API.Community.Domain.Model.Events;
 
-public class PostLikedEvent
+public class PostLikedEvent(int postId, int newLikeCount) : IEvent
 {
-    
+    public int PostId { get; } = postId;
+    public int NewLikeCount { get; } = newLikeCount;
 }
