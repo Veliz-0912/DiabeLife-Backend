82b0ea41e2cf8895c9f1fbb3628ade8cd6c68472
Gabriel Mamani <u202220659@upc.edu.pe>
2025-11-11 21:38:16 -0500

    feat: add comment repository

--- DevsPros.Diabelife.Platform.API/Community/Domain/Repositories/ICommentRepository.cs
+++ DevsPros.Diabelife.Platform.API/Community/Domain/Repositories/ICommentRepository.cs
@@ -1,6 +1,10 @@
+using DevsPros.Diabelife.Platform.API.Community.Domain.Model.Entities;
+
 namespace DevsPros.Diabelife.Platform.API.Community.Domain.Repositories;
 
-public class ICommentRepository
+public interface ICommentRepository
 {
-    
+    Task<IEnumerable<Comment>> ListByPostIdAsync(Guid postId);
+    Task AddAsync(Comment comment);
+    Task SaveChangesAsync();
 }
