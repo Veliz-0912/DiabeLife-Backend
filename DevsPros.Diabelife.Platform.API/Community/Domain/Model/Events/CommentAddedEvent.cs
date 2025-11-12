538f306e8001a66cb1b18b717ee23a6e245e67e8
Gabriel Mamani <u202220659@upc.edu.pe>
2025-11-11 21:11:24 -0500

    feat add: comment added event

--- DevsPros.Diabelife.Platform.API/Community/Domain/Model/Events/CommentAddedEvent.cs
+++ DevsPros.Diabelife.Platform.API/Community/Domain/Model/Events/CommentAddedEvent.cs
@@ -1,6 +1,10 @@
+using DevsPros.Diabelife.Platform.API.Shared.Domain.Model.Events;
+
 namespace DevsPros.Diabelife.Platform.API.Community.Domain.Model.Events;
 
-public class CommentAddedEvent
+public class CommentAddedEvent(int postId, int authorId, string text) : IEvent
 {
-    
+    public int PostId { get; } = postId;
+    public int AuthorId { get; } = authorId;
+    public string Text { get; } = text;
 }
